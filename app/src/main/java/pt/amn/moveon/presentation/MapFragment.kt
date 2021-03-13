e7674c6709e6e9901b5d6dd6591832033cbe9803
Alexander Fokin <alexanderfokindev@gmail.com>
2021-03-13 09:25:58 +0300

    Configured the map fragment and show visited countries on it

--- app/src/main/java/pt/amn/moveon/presentation/MapFragment.kt
+++ app/src/main/java/pt/amn/moveon/presentation/MapFragment.kt
@@ -5,7 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import dagger.hilt.android.AndroidEntryPoint
 import pt.amn.moveon.R
+import pt.amn.moveon.databinding.FragmentMapBinding
+import pt.amn.moveon.domain.models.Country
+import pt.amn.moveon.presentation.viewmodels.MapViewModel
+import pt.amn.moveon.presentation.viewmodels.utils.Status
+import pt.amn.moveon.utils.START_MAP_LATITUDE
+import pt.amn.moveon.utils.START_MAP_LONGITUDE
+import timber.log.Timber
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,7 +35,18 @@ private const val ARG_PARAM2 = "param2"
  * Use the [MapFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class MapFragment : Fragment() {
+@AndroidEntryPoint
+class MapFragment : Fragment(), OnMapReadyCallback {
+
+    private var _binding: FragmentMapBinding? = null
+    // This property is only valid between onCreateView and onDestroyView
+    private val binding get() = _binding!!
+
+    private lateinit var googleMapFragment: SupportMapFragment
+    private lateinit var myMap: GoogleMap
+
+    private val viewModel: MapViewModel by viewModels()
+
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -35,10 +64,74 @@ class MapFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false)
+        _binding = FragmentMapBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        googleMapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        googleMapFragment.getMapAsync(this@MapFragment)
+
+        viewModel.visitedCountries.observe(viewLifecycleOwner, Observer { resCountries ->
+            when (resCountries.status) {
+                Status.SUCCESS -> {
+                    setVisitedCountriesFlags(resCountries.data ?: emptyList())
+                }
+                Status.ERROR -> {
+                    Toast.makeText(requireContext(), resCountries.message, Toast.LENGTH_LONG)
+                        .show()
+                }
+                Status.LOADING -> {
+                }
+            }
+        })
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
+
+    override fun onMapReady(googleMap: GoogleMap?) {
+
+        myMap = googleMap as GoogleMap
+        myMap.moveCamera(
+            CameraUpdateFactory.newLatLng(
+                LatLng(
+                    START_MAP_LATITUDE,
+                    START_MAP_LONGITUDE
+                )
+            )
+        )
+
+    }
+
+    private fun setVisitedCountriesFlags(visitedCountries: List<Country>) {
+        for (country in visitedCountries) {
+            addCountryMarker(country)
+        }
+    }
+
+    private fun addCountryMarker(country: Country) {
+
+        try {
+            myMap.addMarker(
+                MarkerOptions()
+                .position(LatLng(country.latitude, country.longitude))
+                .title(country.getLocalName())
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.map_marker_green)))
+        } catch (ex: Exception) {
+            Timber.d("$TAG, Error of adding country at the map $ex")
+        }
+
     }
 
     companion object {
+
+        private const val TAG = "MapFragment"
+
         /**
          * Use this factory method to create a new instance of
          * this fragment using the provided parameters.
--- app/src/main/java/pt/amn/moveon/utils/Constants.kt
+++ app/src/main/java/pt/amn/moveon/utils/Constants.kt
@@ -2,3 +2,7 @@ package pt.amn.moveon.utils
 
 const val DATABASE_NAME = "moveon_db"
 const val COUNTRIES_DATA_FILENAME = "countries.json"
+
+// Moscow
+const val START_MAP_LATITUDE = 55.7520263
+const val START_MAP_LONGITUDE = 37.6153107
