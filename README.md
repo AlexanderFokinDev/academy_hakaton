1f71c2b4a44ebdbd914100756794b1d6151f8e98
Alexander Fokin <alexanderfokindev@gmail.com>
2022-06-20 18:47:39 +0300

    Created a function for sending a json-data for the backup to any source

--- README.md
+++ README.md
@@ -5,15 +5,14 @@ MoveOn is an application for travelers who want to know how many countries and p
 - quickly mark visited countries in the list
 - if the user wants to mark places visited in the country, he can do it in the country card
 - see visited countries and places on the Google Map
-- the user can save data on one device, and then open it on another (synchronization with the Google Firebase)
-- authenticate by e-mail via the web-service Google Firebase
+- the user can save data on one device, and then open it on another (synchronization throw json file)
 - supported languages: English, Russian
 
 > Note that the app is not ended and will develop. 
 
 ## Future targets
 
-- **Invite friends from Facebook**
+- **Invite friends**
 - **Rating between users**
 - **Useful information about countries: accomodation, transfer, attractions**
 - **New languages: French, Portuguese, Chinese, Hindi**
--- app/src/main/java/pt/amn/moveon/domain/usecases/BackupUseCase.kt
+++ app/src/main/java/pt/amn/moveon/domain/usecases/BackupUseCase.kt
@@ -2,20 +2,31 @@ package pt.amn.moveon.domain.usecases
 
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import androidx.core.content.ContextCompat.startActivity
+import androidx.core.content.FileProvider
 import pt.amn.moveon.domain.repositories.BackupRepository
+import java.io.File
 
 class BackupUseCase(private val repository: BackupRepository) {
 
     fun getBackupData(): String = repository.getBackupDataInJson()
 
     fun sendBackup(context: Context) {
+
+        val backupFile = File(context.filesDir, "backup_moveon.json")
+        backupFile.writeText(getBackupData())
+        val uri = FileProvider.getUriForFile(context, context.applicationContext.packageName
+        + ".provider", backupFile)
+
         val shareIntent: Intent = Intent().apply {
             action = Intent.ACTION_SEND
-            putExtra(Intent.EXTRA_STREAM, getBackupData())
+            putExtra(Intent.EXTRA_SUBJECT, "backup_moveon.json")
+            putExtra(Intent.EXTRA_TEXT, "The backup file is in the attachment")
+            putExtra(Intent.EXTRA_STREAM, uri)
             type = "text/json"
         }
-        startActivity(context, Intent.createChooser(shareIntent, null), null)
+        startActivity(context, Intent.createChooser(shareIntent, "Send a backup file"), null)
     }
 
 }
