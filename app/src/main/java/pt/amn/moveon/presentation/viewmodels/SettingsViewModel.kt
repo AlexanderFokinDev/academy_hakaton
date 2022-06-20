274292b270a709d2b8da38775cdbb07d0f81bf4e
Alexander Fokin <alexanderfokindev@gmail.com>
2022-06-20 23:46:25 +0300

    Fill countries & places  for Json (backup data)

--- app/src/main/java/pt/amn/moveon/presentation/viewmodels/SettingsViewModel.kt
+++ app/src/main/java/pt/amn/moveon/presentation/viewmodels/SettingsViewModel.kt
@@ -2,7 +2,9 @@ package pt.amn.moveon.presentation.viewmodels
 
 import android.content.Context
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import pt.amn.moveon.domain.repositories.BackupRepository
 import pt.amn.moveon.domain.usecases.BackupUseCase
 import javax.inject.Inject
@@ -14,6 +16,8 @@ class SettingsViewModel @Inject constructor(private val repository: BackupReposi
     private val interactor = BackupUseCase(repository)
 
     fun createBackup(context: Context) {
-        interactor.sendBackup(context)
+        viewModelScope.launch {
+            interactor.sendBackup(context)
+        }
     }
 }
