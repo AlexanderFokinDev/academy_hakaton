c6c8862bfede8df1b6026e22be18eafa88822742
Alexander Fokin <alexanderfokindev@gmail.com>
2022-07-04 16:06:51 +0300

    Created a test for CountryDao interface. Some improvements

--- app/src/test/java/pt/amn/moveon/domain/usecases/UpdateStatisticsTest.kt
+++ app/src/test/java/pt/amn/moveon/domain/usecases/UpdateStatisticsTest.kt
@@ -1,14 +1,11 @@
 package pt.amn.moveon.domain.usecases
 
-import android.content.Context
-import androidx.test.core.app.ApplicationProvider
 import org.junit.Assert.*
 import org.junit.Test
 
 class UpdateStatisticsTest {
 
     private val updateStatisticsTest = UpdateStatistics.Base(StatisticsSolver.Base())
-    //private val context = ApplicationProvider.getApplicationContext<Context>()
 
     @Test
     fun checkUpdateStatisticsGetMethod() {
@@ -18,13 +15,5 @@ class UpdateStatisticsTest {
         assertEquals(25, statData.countVisitedPlaces)
         assertEquals(6.153846153846154, statData.percentWorld, 0.0)
     }
-
-    /*@Test
-    fun checkUpdateStatisticsTextConverter() {
-        val statTextData =
-            updateStatisticsTest.getStatisticsOfTraveler(12, 25)
-                .convertToStatisticsOfTravelerText(context)
-        assertEquals("", statTextData.levelText)
-    }*/
 
 }
